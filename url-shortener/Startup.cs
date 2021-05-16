c0ec733863d3af6125903d03f0e6fcad70f08b95
Todd Spatafore <todd@thetodd.com>
2021-05-15 20:14:48 -0700

    Second try at an error page

--- url-shortener/Startup.cs
+++ url-shortener/Startup.cs
@@ -58,7 +58,8 @@ namespace url_shortener
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseDeveloperExceptionPage();
+                // app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
