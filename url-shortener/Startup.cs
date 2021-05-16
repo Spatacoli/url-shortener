991d1c5aaef682458aa40f28263885579f85dcf6
Todd Spatafore <todd@thetodd.com>
2021-05-15 21:45:34 -0700

    Turning dev mode errors back on.

--- url-shortener/Startup.cs
+++ url-shortener/Startup.cs
@@ -57,8 +57,9 @@ namespace url_shortener
                 app.UseDeveloperExceptionPage();
             }
             else
-            { 
-                app.UseExceptionHandler("/Home/Error");
+            {
+                app.UseDeveloperExceptionPage();
+                // app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
