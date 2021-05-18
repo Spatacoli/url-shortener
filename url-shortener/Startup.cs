3e191cb8ce904b68acd0c9dd5d4e64e53e90257a
Todd Spatafore <todd@thetodd.com>
2021-05-17 17:56:56 -0700

    Taking the connection string out.

--- url-shortener/Startup.cs
+++ url-shortener/Startup.cs
@@ -58,8 +58,7 @@ namespace url_shortener
             }
             else
             {
-                app.UseDeveloperExceptionPage();
-                // app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
